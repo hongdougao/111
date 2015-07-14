57fd03425ce77a126a6136b52edeaadf2ed2627f
yangyue <yangyue@bdeiMac.local>
2015-07-14 16:32:04 +0800

    Initial Commit

---
+++ 111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111
+//
+//  Created by yangyue on 15/7/14.
+//  Copyright (c) 2015年 com.yintai.iphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  111
+//
+//  Created by yangyue on 15/7/14.
+//  Copyright (c) 2015年 com.yintai.iphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
